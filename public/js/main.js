4d1d36b9bc5c0c06972b0c79ddd2bc5faacc3b83
Sheerforce <zack.waxler@gmail.com>
2015-10-24 13:31:49 -0400

    Remove notifications

--- public/js/main.js
+++ public/js/main.js
@@ -3,29 +3,24 @@ $(function(){
 	// Load audio file
 	var ding = new Audio('assets/ding.mp3');
 
-	// Set default notifyjs className
-	$.notify.defaults({ className: 'success' });
-
 	// Initialize jQuery-UI on send button
 
 	// Generate a unique UUID,
 	// Create a socket.io instance,
 	// Initialize variables for up/down chat history,
-	// Set notification location,
 	// Store time between messages
 	var		
 		UUID = (Math.random().toString()).substring(2, 10),
 		socket = io(),
 		chatHistory = [],
 		chIndex = 0,
-		notificationPos = 'right top',
 		msgDelay = {this: 0, last: 0};
 
 	var scrollToBottom = function(){
 		window.scrollTo(0, document.body.scrollHeight);
 	}
 
-	window.addEventListener('keydown', function(e){
+	$(window).keydown(function(e){
 		if(e.keyCode == 220){
 			console.log(chatHistory + ' ' + chIndex);
 		}
@@ -43,17 +38,7 @@ $(function(){
 		console.log(content);
 		sender = (content.nick ? content.nick : content.sender)
 		if (sender != UUID && sender != $('#nick').val()) {
-			$.notify(
-				'Message From: ' + sender,
-				{ position: notificationPos }
-			);
 			ding.play();
-		}
-		else {
-			$.notify(
-				'Message Sent',
-				{ position: notificationPos }
-			);
 		}
 		$('#messages').append('<li>' + content.time + ' ' + sender + ' : ' + content.msg + '</li>');
 		scrollToBottom();
@@ -72,15 +57,5 @@ $(function(){
 	});
 	$('#clear').click(function(){
 		$('#messages').empty();
-		$.notify(
-			'Chat Cleared',
-			{ position: notificationPos }
-		);
-	});
-	$('#nick').focusout(function(){
-		$.notify(
-			'Nickname Changed',
-			{ position: notificationPos }
-		);
 	});
 });
