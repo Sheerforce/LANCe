1d2023c45c5897c36e62b461888462754ef24fcd
Sheerforce <zack.waxler@gmail.com>
2015-11-13 09:14:06 -0500

    Add automated install script for chromebooks

---
+++ chromebook.sh
@@ -0,0 +1,4 @@
+echo '################################################################'
+echo '# chromebook.sh from LANCe https://github.com/sheerforce/LANCe #'
+echo '################################################################'
+echo 'Running v1.0...'
