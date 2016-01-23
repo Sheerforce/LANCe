6800c7b8e9bb509da19e384e160d177200709130
BuildTools <unconfigured@null.spigotmc.org>
2016-01-22 22:43:16 -0500

    Remove chromebook.sh

--- chromebook.sh
+++
@@ -1,4 +0,0 @@
-echo '################################################################'
-echo '# chromebook.sh from LANCe https://github.com/sheerforce/LANCe #'
-echo '################################################################'
-echo 'Running v1.0...'
